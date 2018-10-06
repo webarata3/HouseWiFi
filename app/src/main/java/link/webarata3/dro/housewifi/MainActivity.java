ede55de07b3b14072ce72dcd5c8a3fa2f1c4bbfd
webarata3 <webarata3@gmail.com>
2018-10-07 07:18:12 +0900

    最初のコミット

---
+++ app/src/main/java/link/webarata3/dro/housewifi/MainActivity.java
@@ -0,0 +1,13 @@
+package link.webarata3.dro.housewifi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
