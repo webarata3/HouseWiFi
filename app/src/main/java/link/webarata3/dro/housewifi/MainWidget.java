7ec725cbeb2f7fa4bb9b297f25743ce1d389c333
webarata3 <webarata3@gmail.com>
2018-10-08 11:13:25 +0900

    不要な空行の削除

--- app/src/main/java/link/webarata3/dro/housewifi/MainWidget.java
+++ app/src/main/java/link/webarata3/dro/housewifi/MainWidget.java
@@ -65,7 +65,6 @@ public class MainWidget extends AppWidgetProvider {
                                     break;
                                 case UNKNOWN:
                                     break;
-
                             }
                         }
                     }, intentFilter);
