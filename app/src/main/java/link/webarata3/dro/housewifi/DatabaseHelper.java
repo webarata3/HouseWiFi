17b50f3709d9df13c1d8777f7e23c948f452bf33
webarata3 <webarata3@gmail.com>
2018-10-09 18:56:09 +0900

    fix #1 初期データの登録を除去

--- app/src/main/java/link/webarata3/dro/housewifi/DatabaseHelper.java
+++ app/src/main/java/link/webarata3/dro/housewifi/DatabaseHelper.java
@@ -9,13 +9,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
 
-    private static final String[] INIT_DATA = {
-            "INSERT INTO ssid(ssid) VALUES('a_kappa_wifi2')",
-            "INSERT INTO ssid(ssid) VALUES('g_kappa_wifi2')",
-            "INSERT INTO ssid(ssid) VALUES('a_kappa_wifi2f')",
-            "INSERT INTO ssid(ssid) VALUES('g_kappa_wifi2f')"
-    };
-
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -28,10 +21,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + ")";
 
         db.execSQL(ddl);
-
-        for (String initData : INIT_DATA) {
-            db.execSQL(initData);
-        }
     }
 
     @Override
