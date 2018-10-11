e5e890348a3127376a288fa520496b2582b618ce
webarata3 <webarata3@gmail.com>
2018-10-11 17:49:39 +0900

    AppExecutorsの追加（後で使う）

---
+++ app/src/main/java/link/webarata3/dro/housewifi/AppExecutors.java
@@ -0,0 +1,45 @@
+package link.webarata3.dro.housewifi;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import androidx.annotation.NonNull;
+
+public class AppExecutors {
+    private final Executor diskIo;
+    private final Executor mainThread;
+
+    private static AppExecutors appExecutors;
+
+    public static synchronized AppExecutors getInstance() {
+        if (appExecutors == null) {
+            appExecutors = new AppExecutors();
+        }
+        return appExecutors;
+    }
+
+    private AppExecutors() {
+        this.diskIo = Executors.newSingleThreadExecutor();
+        this.mainThread = new MainThreadExecutor();
+    }
+
+    public Executor diskIo() {
+        return diskIo;
+    }
+
+    public Executor mainThread() {
+        return mainThread;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+}
