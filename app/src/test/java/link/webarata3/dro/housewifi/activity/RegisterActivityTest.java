b0d96045d3a23cf024555dc71b2b36b6b1341504
webarata3 <webarata3@gmail.com>
2018-11-07 19:36:43 +0900

    importの編成

--- app/src/test/java/link/webarata3/dro/housewifi/activity/RegisterActivityTest.java
+++ app/src/test/java/link/webarata3/dro/housewifi/activity/RegisterActivityTest.java
@@ -9,6 +9,7 @@ import link.webarata3.dro.housewifi.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -16,20 +17,16 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class RegisterActivityTest {
     @Test
-    public void test() {
+    public void test_clickRegisterButton_error() {
         ActivityScenario.launch(RegisterActivity.class);
         onView(withId(R.id.registerButton)).perform(click());
         onView(withId(R.id.ssidEditText)).check(matches(hasErrorText("入力必須です")));
+    }
 
-//        Intent result = new Intent();
-//
-//        intending(toPackage("link.webarata3.dro.housewifi.acitivity")).respondWith();
-//
-//        intending(toPackage("package.containing.login.activity")).respondWith(result);
-//
-//        intended(allOf(
-//                hasComponent(hasMyPackageName()),
-//                hasComponent(hasClassName("link.webarata3.dro.housewifi.activity.RegisterActivity"))
-//        ));
+    @Test
+    public void test_clickRegisterbutton_intent() {
+        ActivityScenario.launch(RegisterActivity.class);
+        onView(withId(R.id.ssidEditText)).perform(replaceText("dummy_ssid"));
+        onView(withId(R.id.registerButton)).perform(click());
     }
 }
