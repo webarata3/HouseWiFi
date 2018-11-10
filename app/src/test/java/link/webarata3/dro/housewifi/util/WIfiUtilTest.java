940afce5c4cf6168302b50bb9967f79aac989b22
webarata3 <webarata3@gmail.com>
2018-11-10 13:54:52 +0900

    WifiUtilTest.test_getCurrentAccessPoint

--- app/src/test/java/link/webarata3/dro/housewifi/util/WIfiUtilTest.java
+++ app/src/test/java/link/webarata3/dro/housewifi/util/WIfiUtilTest.java
@@ -1,13 +1,19 @@
 package link.webarata3.dro.housewifi.util;
 
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import link.webarata3.dro.housewifi.model.AccessPoint;
 import link.webarata3.dro.housewifi.model.ConnectedWifi;
 
 import static android.content.Context.*;
@@ -43,5 +49,35 @@ public class WIfiUtilTest {
         assertThat(connectedWifi.getSsid(), is("dummy_ssid"));
         assertThat(connectedWifi.getIpAddress(), is("192.168.123.45"));
         assertThat(connectedWifi.getLinkSpeed(), is(866));
+    }
+
+    @Test
+    public void test_getCurrentAccessPoint() {
+        Context context = mock(Context.class);
+        Context applicationContext = mock(Context.class);
+
+        when(context.getApplicationContext()).thenReturn(applicationContext);
+
+        WifiManager mockWifiManager = mock(WifiManager.class);
+        when(applicationContext.getSystemService(WIFI_SERVICE)).thenReturn(mockWifiManager);
+
+        when(mockWifiManager.startScan()).thenReturn(true);
+
+        ScanResult mockScanResult = mock(ScanResult.class);
+        mockScanResult.SSID = "dummy_ssid";
+        mockScanResult.level = -50;
+
+        List<ScanResult> mockList = new ArrayList<>();
+        mockList.add(mockScanResult);
+
+        when(mockWifiManager.getScanResults()).thenReturn(mockList);
+
+        Map<String, AccessPoint> resultMap = WifiUtil.getCurrentAccessPoint(context);
+
+        assertThat(resultMap.size(), is(1));
+        AccessPoint accessPoint = resultMap.get(0);
+        assertThat(accessPoint, is(notNullValue()));
+        assertThat(accessPoint.getSsid(), is("dummy_ssid"));
+        assertThat(accessPoint.getQuality(), is(-50));
     }
 }
