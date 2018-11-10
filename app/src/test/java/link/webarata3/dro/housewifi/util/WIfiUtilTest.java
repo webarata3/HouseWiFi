f56fb85a993dbd144770153c2fc349cb2b2f0943
webarata3 <webarata3@gmail.com>
2018-11-10 11:50:13 +0900

    WifiUtilTest.test_getConnectedWifi

--- app/src/test/java/link/webarata3/dro/housewifi/util/WIfiUtilTest.java
+++ app/src/test/java/link/webarata3/dro/housewifi/util/WIfiUtilTest.java
@@ -4,30 +4,30 @@ import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import link.webarata3.dro.housewifi.model.ConnectedWifi;
 
-import static android.content.Context.WIFI_SERVICE;
+import static android.content.Context.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(AndroidJUnit4.class)
 public class WIfiUtilTest {
-    private Context context;
-
-    @Before
-    public void setUp() {
-        context = ApplicationProvider.getApplicationContext();
-    }
-
     @Test
     public void test_getConnectedWifi() {
-        WifiManager spyWifiManager = (WifiManager) spy(context.getSystemService(WIFI_SERVICE));
-        WifiInfo mockWifiInfo = spy(spyWifiManager.getConnectionInfo());
+        Context context = mock(Context.class);
+        Context applicationContext = mock(Context.class);
+
+        when(context.getApplicationContext()).thenReturn(applicationContext);
+
+        WifiManager mockWifiManager = mock(WifiManager.class);
+        when(applicationContext.getSystemService(WIFI_SERVICE)).thenReturn(mockWifiManager);
+
+        WifiInfo mockWifiInfo = mock(WifiInfo.class);
 
         when(mockWifiInfo.getSSID()).thenReturn("dummy_ssid");
         int ip = 45 << 8;
@@ -37,6 +37,11 @@ public class WIfiUtilTest {
         when(mockWifiInfo.getIpAddress()).thenReturn(ip);
         when(mockWifiInfo.getLinkSpeed()).thenReturn(866);
 
+        when(mockWifiManager.getConnectionInfo()).thenReturn(mockWifiInfo);
+
         ConnectedWifi connectedWifi = WifiUtil.getConnectedWifi(context);
+        assertThat(connectedWifi.getSsid(), is("dummy_ssid"));
+        assertThat(connectedWifi.getIpAddress(), is("192.168.123.45"));
+        assertThat(connectedWifi.getLinkSpeed(), is(866));
     }
 }
